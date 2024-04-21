4b1ada2e422ecf23f73268ccc10753373eb89bab
guteh <83144896+guteh@users.noreply.github.com>
2024-04-21 16:56:42 -0400

    Agregado mutex para concurrencia

--- Central/Central.go
+++ Central/Central.go
@@ -6,15 +6,18 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
 	"time"
 
 	"google.golang.org/grpc"
 )
+var mutex = &sync.Mutex{} //Se crea un mutex para evitar problemas de concurrencia
 
 func generarRecursos(recursos map[string]int) {  //Genera recursos cada 5 segundos
 	for {
 		time.Sleep(5 * time.Second) //Espera 5 segundos
 
+		mutex.Lock() // Bloquea recursos
 		recursos["AT"] += 10  //Genera recursos AT y MP
 		recursos["MP"] += 5
 
@@ -26,6 +29,7 @@ func generarRecursos(recursos map[string]int) {  //Genera recursos cada 5 segund
 		if recursos["MP"] > 20 { //Si los recursos generados son mayor a 20, se asigna 20
 			recursos["MP"] = 20
 		}
+		mutex.Unlock() // Desbloquea recursos
 		
 		//fmt.Printf("Recursos generados: AT: %d, MP: %d\n", recursos["AT"], recursos["MP"])
 	}
@@ -41,11 +45,13 @@ type server struct {  //Crea el servidor rcp
 //Implementa la funcion SolicitarM de la interfaz ServicioRecursos de RCP
 
 func (s *server) SolicitarM(ctx context.Context, req *pb.ResourceRequest) (*pb.ResourceResponse, error) {
+	mutex.Lock() //Bloquea recursos
+
 
     if s.recursos["AT"] >= int(req.GetAT()) && s.recursos["MP"] >= int(req.GetMP()) {  //Si los recursos son suficientes
-
         s.recursos["AT"] -= int(req.GetAT())  //Se restan los recursos asignados
         s.recursos["MP"] -= int(req.GetMP())
+
 
 		fmt.Printf("Recepcion de solicitud desde equipo %d, %d AT y %d MP -- APROBADA --\nAT EN SISTEMA: %d ; MP EN SISTEMA: %d \n", req.GetID(), req.GetAT(), req.GetMP(), s.recursos["AT"], s.recursos["MP"])
 		fmt.Println("\n")
@@ -56,11 +62,15 @@ func (s *server) SolicitarM(ctx context.Context, req *pb.ResourceRequest) (*pb.R
 				s.grpcServer.Stop()
 			}()
 		}
+
+		mutex.Unlock() //Desbloquea recursos dando paso a otro proceso
         return &pb.ResourceResponse{Message: 1}, nil //Se retorna en funcion SolicitarM un mensaje de aprobacion
+
     } else {
         // No hay suficientes recursos
 		fmt.Printf("Recepcion de solicitud desde equipo %d, %d AT y %d MP -- DENEGADA --\nAT EN SISTEMA: %d ; MP EN SISTEMA: %d \n", req.GetID(), req.GetAT(), req.GetMP(), s.recursos["AT"], s.recursos["MP"])
 		fmt.Println("\n")
+		mutex.Unlock()//Desbloquea recursos dando paso a otro proceso
         return &pb.ResourceResponse{Message: 0}, nil  //Se retorna en funcion SolicitarM un mensaje de denegacion
     }
 }
@@ -69,20 +79,24 @@ func (s *server) SolicitarM(ctx context.Context, req *pb.ResourceRequest) (*pb.R
 
 
 func main() {
+
 	recursos := make(map[string]int) //Se inicializan recursos
 	recursos["AT"] = 0
 	recursos["MP"] = 0
+
 
 	// Cada 5 segundos se genereran 10 AT y 5 MP los cuales seran almacenados en bodega, el maximo que se puede almacenar es 50 AT y 20 MP
 	go generarRecursos(recursos)
 
 	grpcServer := grpc.NewServer() //Se crea el servidor
 
+	mutex.Lock()
 	s := &server{
         recursos: recursos, //Se le asignan los recursos al servidor
 		contador: 0,
 		grpcServer: grpcServer,
     }
+	mutex.Unlock()
 
 	pb.RegisterServicioRecursosServer(grpcServer, s) //Se registra el servidor
 	
